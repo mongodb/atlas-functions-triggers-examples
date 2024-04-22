c83c449d0eb99f3f08c9090a7cb8c2f7f0ca7d47
Joey Wong <joey.wong@mongodb.com>
2024-04-22 16:52:55 -0400

    fix snippet from being nested in object

--- snippets/third-party/thirdParty_twilio.js
+++ snippets/third-party/thirdParty_twilio.js
@@ -1,15 +1,13 @@
-{
-  exports = async function () {
-    var accountSid = context.values.get("TwilioAccountSID");
-    var authToken = context.values.get("TwilioAuthToken");
+exports = async function () {
+  var accountSid = context.values.get("TwilioAccountSID");
+  var authToken = context.values.get("TwilioAuthToken");
 
-    const client = require("twilio")(accountSid, authToken);
+  const client = require("twilio")(accountSid, authToken);
 
-    const messages = await client.messages.create({
-      body: "hello world",
-      from: "+15005550006",
-      to: "+14242424242",
-    });
-    return messages.accountSid;
-  };
-}
+  const messages = await client.messages.create({
+    body: "hello world",
+    from: "+15005550006",
+    to: "+14242424242",
+  });
+  return messages.accountSid;
+};
