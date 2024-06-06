5187dcf0845a7796a5558b77f9f084b848bcc5b7
Joey Wong <joey.wong@mongodb.com>
2024-06-06 15:15:16 -0400

    use better test endpoint

--- snippets/functions/third-party/axios.js
+++ snippets/functions/third-party/axios.js
@@ -1,7 +1,10 @@
 exports = async function () {
   const axios = require("axios").default;
-  const response = await axios.get("https://api.github.com/users/octocat", {
-    headers: { "Content-Type": "application/json" },
-  });
-  return response.data.login;
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts/2",
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  return response.data.userId;
 };
